9594e6d908877d5025863a19f04d6cf80d1f2f69
Freivin Campbell <freivin12@gmail.com>
2021-09-16 23:38:16 -0600

    New route to get all users from API
    New route to get all users from API

---
+++ infrastructure/router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"academy-go-q32021/interface/controller"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func NewRouter(e *echo.Echo, c controller.App) *echo.Echo {
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/users", func(context echo.Context) error { return c.User.GetUsers(context) })
+
+	return e
+}
