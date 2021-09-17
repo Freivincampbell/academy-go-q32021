466831d9fd19b1946fbae5a27777c6812d580853
Freivin Campbell <freivin12@gmail.com>
2021-09-17 12:33:42 -0600

    Update user.go

--- interface/presenter/user.go
+++ interface/presenter/user.go
@@ -1,20 +1,16 @@
 package presenter
 
-import (
-	"academy-go-q32021/domain/model"
-)
-
 type user struct {
 }
 
 type User interface {
-	ResponseUsers(us []*model.User) []*model.User
+	ResponseReadUsers(f string) string
 }
 
 func NewUserPresenter() User {
 	return &user{}
 }
 
-func (up *user) ResponseUsers(us []*model.User) []*model.User {
-	return us
+func (up *user) ResponseReadUsers(f string) string {
+	return f
 }
